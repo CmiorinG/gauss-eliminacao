a2c54d115ffe5adbd188d09a6b4f93c8bebc04da
Your Name <cmioring@edu.unifil.br>
2023-05-29 21:35:54 -0300

    Update gauss.py
    com tabela

--- gauss.py
+++ gauss.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib as mt
+import streamlit as st
+import pandas as pd
 
 
 def eliminacao_gauss(matriz):
@@ -32,9 +34,31 @@ matriz = np.array([[7, 3, 1, 4],
                    [3, 2, -3, -2],
                    [5, 2, 5, 10]])  #matriz para teste
 
-resultado = eliminacao_gauss(matriz)
-print("Matriz resultante:")
-print(resultado)
+
+
+st.sidebar.title("Seleção de Sistema Linear")
+
+genre = st.sidebar.radio(
+    "Qual sistema linear você deseja calcular?",
+    ('Gauss', 'Gauss - Seidel'))
+
+rows = st.sidebar.number_input("Número de linhas da matriz", min_value=1, value=3)
+cols = rows
+
+if genre == 'Gauss':
+    st.write('Você selecionou Gauss.')
+else:
+    st.write("Você selecionou Gauss - Seidel")
+
+st.write(f"Número de linhas e colunas selecionadas: {rows}")
+
+data = [[st.sidebar.number_input(f"Valor [{i+1},{j+1}]", key=f"value_{i}_{j}", value=0.0) for j in range(cols)] for i in range(rows)]
+df = pd.DataFrame(data, index=range(1, rows + 1), columns=range(1, cols + 1))
+edited_df = st.experimental_data_editor(df)
+
+
+
+
 
 #n = matriz 
 #i = numero
